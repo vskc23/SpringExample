d38b5ef984dfaaeb0a1b223e069ea51967c049f9
vskc23 <kckrishna234@gmail.com>
2020-10-19 16:55:47 +0530

    added greeting

--- SpringBootActuator/src/main/java/com/training/org/LoggingController.java
+++ SpringBootActuator/src/main/java/com/training/org/LoggingController.java
@@ -2,8 +2,12 @@ package com.training.org;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class LoggingController {
@@ -20,4 +24,22 @@ public class LoggingController {
     
     return "Welcome to Spring Logging! Check the console to see the log messages.";
   }
+  
+  @RequestMapping("/greeting")
+  public ModelAndView welcome() {
+      ModelAndView modelAndView = new ModelAndView();
+      modelAndView.setViewName("Greeting.html");
+      return modelAndView;
+  }
+  
+  @GetMapping(value = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
+  @ResponseBody
+  public String welcomeAsHTML() {
+      return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+        "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+  }
+
+  
+  
+  
 }
